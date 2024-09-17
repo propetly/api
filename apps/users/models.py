338f8a45fd83c5d57d148127b1445f87d53f2cea
Nick Risinger <boozy044@gmail.com>
2024-09-17 15:33:09 +0300

    feat(users): add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} <{self.email}>"
