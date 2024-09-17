acaed5aae2eee238b1bb1d1f5785158281972ac3
Nick Risinger <boozy044@gmail.com>
2024-09-17 16:10:32 +0300

    feat(users): add user endpoints

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
