efe7fbaf957e7e33193b8a70be9b9ff21b78b105
Nick Risinger <boozy044@gmail.com>
2024-09-17 18:11:10 +0300

    feat(contractors): add contractor model

--- apps/contractors/models.py
+++ apps/contractors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contractor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+
+    agency = models.ForeignKey("agencies.Agency", on_delete=models.CASCADE, related_name="contractors")
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
