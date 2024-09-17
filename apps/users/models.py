fefbc032e0077e11a29a97cde6ad41e6e63c08bf
Nick Risinger <boozy044@gmail.com>
2024-09-17 16:44:24 +0300

    feat(users): add relations for agency

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.agencies.models import Agency
 
 
 class User(models.Model):
@@ -6,6 +7,8 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)
     second_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+
+    agency = models.ForeignKey(Agency, on_delete=models.CASCADE, related_name="users")
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} <{self.email}>"
