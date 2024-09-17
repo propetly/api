827d8fb86cf4a434476f5f3b612b7800f9f03a1c
Nick Risinger <boozy044@gmail.com>
2024-09-17 16:38:30 +0300

    feat(agencies): add agency model

--- apps/agencies/models.py
+++ apps/agencies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Agency(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    watermark = models.CharField(max_length=255, blank=True, null=True)
+
+    director_first_name = models.CharField(max_length=255, blank=True, null=True)
+    director_last_name = models.CharField(max_length=255, blank=True, null=True)
+    director_middle_name = models.CharField(max_length=255, blank=True, null=True)
+    director_email = models.EmailField(blank=True, null=True)
+    director_phone = models.CharField(max_length=255, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
